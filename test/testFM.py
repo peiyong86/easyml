a913663bd89ead7a0b9ec892856a15a27dfe5c88
peiyong86 <peiyong86@gmail.com>
2020-01-21 15:06:36 +0800

    Update tests, add evaluation and assertion.

--- test/testFM.py
+++ test/testFM.py
@@ -11,12 +11,12 @@ from easyml import FM, FMParam, DataSet
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_fm(self):
+    def test_fm_AdaDelta_Logloss(self):
         param = FMParam(learning_rate=0.01,
                         embed_size=10,
                         decay=0.8,
                         decay_step=3,
-                        epochs=10,
+                        epochs=5,
                         batch_size=100,
                         regW=0.0,
                         regV=0.01,
@@ -26,6 +26,30 @@ class TestStringMethods(unittest.TestCase):
         dataset = DataSet()
         dataset.loaddata('./data/breast_data.txt')
         model.fit(dataset)
+        eval_re = model.evaluate(dataset)
+
+        self.assertGreater(eval_re['auc'], 0.9)
+        self.assertGreater(eval_re['accuracy'], 0.9)
+
+    def test_fm_SGD_MSELoss(self):
+        param = FMParam(learning_rate=0.01,
+                        embed_size=10,
+                        decay=0.8,
+                        decay_step=3,
+                        epochs=10,
+                        batch_size=100,
+                        regW=0.0,
+                        regV=0.01,
+                        loss='mse',
+                        opt='SGD')
+        model = FM(param)
+        dataset = DataSet()
+        dataset.loaddata('./data/breast_data.txt')
+        model.fit(dataset)
+        eval_re = model.evaluate(dataset)
+
+        self.assertGreater(eval_re['auc'], 0.9)
+        self.assertGreater(eval_re['accuracy'], 0.9)
 
     # def test_isupper(self):
     #     self.assertTrue('FOO'.isupper())
